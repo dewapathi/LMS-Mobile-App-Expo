3b40f6acfa527f37ed132234efb24cd6667b614d
Pradeepa Lakruwan <pradeepa@creatit.com.au>
2025-06-01 09:00:23 +0530

    Removed the tabs from bottom.

--- app/(auth)/(admin)/_layout.tsx
+++ app/(auth)/(admin)/_layout.tsx
@@ -1,30 +1,42 @@
-import { Tabs } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 // import { useAuthStore } from '@/stores/auth.store';
 import { MaterialIcons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function AdminLayout() {
-  const colorScheme = useColorScheme();
-  //   const { role } = useAuthStore();
-  const role = "admin";
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          title: "Dashboard",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="dashboard" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
+// export default function AdminLayout() {
+//   const colorScheme = useColorScheme();
+//   //   const { role } = useAuthStore();
+//   const role = "admin";
+
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+//         headerShown: false,
+//       }}
+//     >
+//       <Tabs.Screen
+//         name="dashboard"
+//         options={{
+//           title: "Dashboard",
+//           tabBarIcon: ({ color }) => (
+//             <MaterialIcons name="dashboard" size={24} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="users"
+//         options={{
+//           title: "User",
+//           tabBarIcon: ({ color }) => (
+//             <MaterialIcons name="dashboard" size={24} color={color} />
+//           ),
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
